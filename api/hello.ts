bb25fd963ced7ca011d009a0ca3759c015f86f2a
Alexei <alexeimyshkouski@gmail.com>
2025-01-04 02:55:13 +0300

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,5 @@
-#!/usr/bin/env deno run --version v2.1.4
+import { createHttpClient } from "https://deno.land/std@0.154.0/_deno_unstable.ts";
+
+const client = createHttpClient({});
 
 export default () => new Response(`Hello, from Deno v${Deno.version.deno}!`);
