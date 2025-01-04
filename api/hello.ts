182da0d602b42489993bb77785a2c5e3deb211c3
Alexei <alexeimyshkouski@gmail.com>
2025-01-04 03:07:53 +0300

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -2,8 +2,12 @@
 	
 import { createHttpClient } from "https://deno.land/std@0.154.0/_deno_unstable.ts";
 
-const key = "----BEGIN PRIVATE KEY----...";
-const cert = "----BEGIN CERTIFICATE----...";
-const client = createHttpClient({ key, cert });
+// const key = "----BEGIN PRIVATE KEY----...";
+// const cert = "----BEGIN CERTIFICATE----...";
+// const client = createHttpClient({ key, cert });
+const client = createHttpClient();
 
-export default () => new Response(`Hello, from Deno v${Deno.version.deno}!`);
+export default () => {
+	const response = await fetch("https://ipapi.co/json")
+	return response
+};
